2400b462840e9bc517393f06c2b2068d1f22eab2
Daffa Ridho Fadhlika <daffa.cm7@gmail.com>
2012-09-05 17:45:11 +0700

    update android.mk
    test for camera hal 2.6

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE_TAGS    := optional
 LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE         := camera.$(TARGET_BOARD_PLATFORM)
 
-LOCAL_PRELINK_MODULE := false
+LOCAL_PRELINK_MODULE := android.mk
 
 LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware libcamera libui
 LOCAL_C_INCLUDES       := frameworks/av/include frameworks/base/include frameworks/native/include
